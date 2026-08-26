c054a0e4c8e9262169a57a4a6456ea7d79c48965
unknown <samiraa.vaeidi@gmail.com>
2026-08-26 21:04:40 +0330

    edit build err

--- utils/auth.js
+++ utils/auth.js
@@ -69,3 +69,13 @@ export const verifyToken = (token) => {
     return null;
   }
 };
+export const getTodayDate = () => {
+  const today = new Date();
+
+  return new Intl.DateTimeFormat("fa-IR-u-ca-persian", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(today);
+};
